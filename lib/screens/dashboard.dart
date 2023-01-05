b47df70cc3a9f8c9b49ac0c2c25e552146ac2828
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-01-05 20:36:37 +0500

    Minor Changes

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import '../widgets/horiontal_card.dart';
+import '../widgets/horizontal_card.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -16,18 +17,18 @@ class _DashboardState extends State<Dashboard> {
     super.initState();
     initialization();
   }
+
   void initialization() async {
     await Future.delayed(const Duration(seconds: 3));
     FlutterNativeSplash.remove();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text(
-            "Expense Dashboard"
-          ),
+          title: const Text("Expense Dashboard"),
         ),
         body: Column(
           children: <Widget>[
@@ -36,16 +37,16 @@ class _DashboardState extends State<Dashboard> {
               child: ListView.builder(
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
-                itemCount: 12,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                        leading: Text((index+1).toString()),
-                        trailing: Text(
-                          "9999 PKR",
-                          style: TextStyle(color: Colors.green, fontSize: 15),
-                        ),
-                        title: Text("Expense - $index"));
-                  },
+                itemCount: 22,//dataa["index"].length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      leading: Text((index + 1).toString()),
+                      trailing: Text(
+                        "9999 PKR",
+                        style: TextStyle(color: Colors.green, fontSize: 15),
+                      ),
+                      title: Text("name aega"));
+                },
               ),
             ),
           ],
