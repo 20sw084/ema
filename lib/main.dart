8363d338f8487a612c329db8a0077202ff22cf96
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-02-25 15:04:55 +0500

    State issue resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:ema/provider/amount_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'screens/my_app.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -11,14 +9,7 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => AmountProvider(),
-        ),
-      ],
-      child: MyApp(),
-    ),
+    MyApp(),
     // const MyApp(),
   );
 }
