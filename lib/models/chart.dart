112a26d479ee8e566b19121e54723c4b0f3be9d8
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-03-07 23:52:31 +0500

    chrts

---
+++ lib/models/chart.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Chart {
+  Chart(this.username, this.amount, this.color);
+  final String username;
+  final double amount;
+  // final DateTime date;
+  final Color color;
+}
--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,19 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import '../models/chart.dart';
 import '../utils/utils.dart';
+import 'dart:math' as math;
+import 'dart:developer';
 
-class ChartScreen extends StatelessWidget {
+class ChartScreen extends StatefulWidget {
   const ChartScreen({Key? key}) : super(key: key);
 
   @override
+  State<ChartScreen> createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  // late List<Chart> _expense = [];
+
+  @override
   Widget build(BuildContext context) {
-    final List<ChartData> chartData = [
-      ChartData('David', 15, Colors.pinkAccent),
-      ChartData('Steve', 8, Colors.purple),
-      ChartData('Jack', 14, Colors.green),
-      ChartData('Others', 2, Colors.grey)
+    final List<Chart> expenseChartData = [
+      Chart('Jos Buttler', 15, Colors.pinkAccent),
+      Chart('Ben Stokes', 8, Colors.purple),
+      Chart('Joe Root', 14, Colors.green),
+      Chart('Others', 2, Colors.grey)
     ];
+    final List<Chart> incomeChartData = [
+      Chart('Tim David', 15, Colors.pinkAccent),
+      Chart('Rilee Roussow', 8, Colors.purple),
+      Chart('Will Jacks', 14, Colors.green),
+      Chart('Others', 2, Colors.grey)
+    ];
+    // List<Color> colorList = [
+    //   Color.fromRGBO(82, 98, 255, 1),
+    //   Color.fromRGBO(46, 198, 255, 1),
+    //   Color.fromRGBO(123, 201, 82, 1),
+    //   Color.fromRGBO(255, 171, 67, 1),
+    //   Color.fromRGBO(252, 91, 57, 1),
+    //   Color.fromRGBO(139, 135, 130, 1),
+    // ];
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -31,11 +55,11 @@ class ChartScreen extends StatelessWidget {
                   child: SfCircularChart(
                     series: <CircularSeries>[
                       // Render pie chart
-                      PieSeries<ChartData, String>(
-                          dataSource: chartData,
-                          pointColorMapper: (ChartData data, _) => data.color,
-                          xValueMapper: (ChartData data, _) => data.x,
-                          yValueMapper: (ChartData data, _) => data.y),
+                      PieSeries<Chart, String>(
+                          dataSource: expenseChartData,
+                          pointColorMapper: (Chart data, _) => data.color,
+                          xValueMapper: (Chart data, _) => data.username,
+                          yValueMapper: (Chart data, _) => data.amount),
                     ],
                   ),
                 ),
@@ -109,28 +133,67 @@ class ChartScreen extends StatelessWidget {
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Expense", style: Utils.ts1(context),),
-            ),
-            Container(
-              child: SfCircularChart(
-                series: <CircularSeries>[
-                  // Render pie chart
-                  PieSeries<ChartData, String>(
-                      dataSource: chartData,
-                      pointColorMapper: (ChartData data, _) => data.color,
-                      xValueMapper: (ChartData data, _) => data.x,
-                      yValueMapper: (ChartData data, _) => data.y),
-                ],
-              ),
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Text("Expense", style: Utils.ts1(context),),
+            // ),
+            // Container(
+            //   child: SfCircularChart(
+            //     series: <CircularSeries>[
+            //       // Render pie chart
+            //       PieSeries<Chart, String>(
+            //           dataSource: incomeChartData,
+            //           pointColorMapper: (Chart data, _) => data.color,
+            //           xValueMapper: (Chart data, _) => data.username,
+            //           yValueMapper: (Chart data, _) => data.amount),
+            //     ],
+            //   ),
+            // ),
+            FutureBuilder(
+                future: getExpenseList(),
+              builder: (context, snapshot) {
+                List<Widget> children = [];
+                if (snapshot.hasData) {
+                  log(snapshot.toString());
+                } else if(snapshot.hasError){
+                  children = const <Widget>[
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 16),
+                      child: Text('Error in result...'),
+                    ),
+                  ];
+                }
+                else {
+                  children = const <Widget>[
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 16),
+                      child: Text('Awaiting result...'),
+                    ),
+                  ];
+                }
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: children,
+                  ),
+                );
+              },
             ),
           ],
         ),
       ),
     );
   }
-
 
   Future getExpenseList() async {
     late var lst = [];
@@ -139,19 +202,34 @@ class ChartScreen extends StatelessWidget {
     await fireStore.get().then(
           (res) {
         res.docChanges.forEach((change) {
-          lst.add(change);
+          (int.parse(change.doc['amount']) > 0)
+              ? null
+              : lst.add(Chart(change.doc["userName"], change.doc["amount"], Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0)));
+
           print(change.toString());
         },);
       },
       onError: (e) => print("Error completing: $e"),
     );
+    print(lst.toString());
     return lst;
   }
+
+
+// Map<String, String> getUserData() {
+//   Map<String, String> catMap = {};
+//   for (var item in _expense) {
+//     print(item.username);
+//     if (catMap.containsKey(item.username) == false) {
+//       catMap[item.username] = 1;
+//     } else {
+//       catMap.update(item.username, (int) => catMap[item.username]! + 1);
+//       // test[item.category] = test[item.category]! + 1;
+//     }
+//     print(catMap);
+//   }
+//   return catMap;
+// }
 }
 
-class ChartData {
-  ChartData(this.x, this.y, this.color);
-  final String x;
-  final double y;
-  final Color color;
-}
+
