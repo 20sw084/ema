9c4198158804e995d934b39838a5adeb33645c1d
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-03-23 19:38:23 +0500

    chrts

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,12 +1,10 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import '../models/chart.dart';
+import '../models/expense.dart';
+import '../models/income.dart';
 import '../utils/utils.dart';
 import 'dart:math' as math;
-import 'dart:developer';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
 class ChartScreen extends StatefulWidget {
@@ -15,13 +13,27 @@ class ChartScreen extends StatefulWidget {
   @override
   State<ChartScreen> createState() => _ChartScreenState();
 }
+
 // Expense Chart Works
-// TODO: Only show statistics of top 3 else others.
-// TODO: Color Should be matched with color identification section.
-// TODO: Pass Color to object via Firebase Data Snapshot
+// TODO: Exception caught when users less than 4
+
 class _ChartScreenState extends State<ChartScreen> {
   // late List<Chart> _expense = [];
-
+  List<Color> colorList = [
+    Colors.blue,
+    Colors.greenAccent,
+    Colors.purple,
+    Colors.red,
+    Colors.grey,
+    Colors.green,
+    Colors.pinkAccent,
+    Colors.yellow,
+    Colors.brown,
+    Colors.deepOrangeAccent,
+    Colors.teal,
+    Colors.lime,
+    Colors.black
+  ];
   @override
   Widget build(BuildContext context) {
     final List<Expense> expenseChartData = [
@@ -36,14 +48,7 @@ class _ChartScreenState extends State<ChartScreen> {
       Expense('Will Jacks', 14, Colors.green),
       Expense('Others', 2, Colors.grey)
     ];
-    // List<Color> colorList = [
-    //   Color.fromRGBO(82, 98, 255, 1),
-    //   Color.fromRGBO(46, 198, 255, 1),
-    //   Color.fromRGBO(123, 201, 82, 1),
-    //   Color.fromRGBO(255, 171, 67, 1),
-    //   Color.fromRGBO(252, 91, 57, 1),
-    //   Color.fromRGBO(139, 135, 130, 1),
-    // ];
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -58,90 +63,189 @@ class _ChartScreenState extends State<ChartScreen> {
                 style: Utils.ts1(context),
               ),
             ),
-            Row(
-              children: [
-                Container(
-                  child: SfCircularChart(
-                    series: <CircularSeries>[
-                      // Render pie chart
-                      PieSeries<Expense, String>(
-                          dataSource: expenseChartData,
-                          pointColorMapper: (Expense data, _) => data.color,
-                          xValueMapper: (Expense data, _) => data.username,
-                          yValueMapper: (Expense data, _) => data.amount),
-                    ],
-                  ),
-                ),
-                Column(
-                  children: [
-                    Row(
+            // Row(
+            //   children: [
+            //     Container(
+            //       child: SfCircularChart(
+            //         series: <CircularSeries>[
+            //           // Render pie chart
+            //           PieSeries<Expense, String>(
+            //               dataSource: expenseChartData,
+            //               pointColorMapper: (Expense data, _) => data.color,
+            //               xValueMapper: (Expense data, _) => data.username,
+            //               yValueMapper: (Expense data, _) => data.amount),
+            //         ],
+            //       ),
+            //     ),
+            //     Column(
+            //       children: [
+            //         Row(
+            //           children: [
+            //             Container(
+            //               height: 20,
+            //               width: 20,
+            //               color: Colors.pink,
+            //             ),
+            //             SizedBox(
+            //               width: 10,
+            //             ),
+            //             Text("Pink"),
+            //           ],
+            //         ),
+            //         SizedBox(
+            //           height: 10.0,
+            //         ),
+            //         Row(
+            //           children: [
+            //             Container(
+            //               height: 20,
+            //               width: 20,
+            //               color: Colors.green,
+            //             ),
+            //             SizedBox(
+            //               width: 10,
+            //             ),
+            //             Text("Green"),
+            //           ],
+            //         ),
+            //         SizedBox(
+            //           height: 10.0,
+            //         ),
+            //         Row(
+            //           children: [
+            //             Container(
+            //               height: 20,
+            //               width: 20,
+            //               color: Colors.purple,
+            //             ),
+            //             SizedBox(
+            //               width: 10,
+            //             ),
+            //             Text("Purple"),
+            //           ],
+            //         ),
+            //         SizedBox(
+            //           height: 10.0,
+            //         ),
+            //         Row(
+            //           children: [
+            //             Container(
+            //               height: 20,
+            //               width: 20,
+            //               color: Colors.grey,
+            //             ),
+            //             SizedBox(
+            //               width: 10,
+            //             ),
+            //             Text("Grey"),
+            //           ],
+            //         ),
+            //         SizedBox(
+            //           height: 10.0,
+            //         ),
+            //       ],
+            //     ),
+            //   ],
+            // ),
+            FutureBuilder(
+                future: getIncomeList(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Row(
                       children: [
                         Container(
-                          height: 20,
-                          width: 20,
-                          color: Colors.pink,
+                          child: SfCircularChart(
+                            series: <CircularSeries>[
+                              // Render pie chart
+                              PieSeries<Income, String>(
+                                // dataSource: expenseChartData,
+                                  dataSource: snapshot.data,
+                                  pointColorMapper: (Income data, _) =>
+                                  data.color,
+                                  xValueMapper: (Income data, _) =>
+                                  data.username,
+                                  yValueMapper: (Income data, _) =>
+                                  data.amount),
+                            ],
+                          ),
                         ),
-                        SizedBox(
-                          width: 10,
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  color: snapshot.data[0].color,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(snapshot.data[0].username),
+                                // Text(snapshot.data["userName"]),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  color: snapshot.data[1].color,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(snapshot.data[1].username),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  color: snapshot.data[2].color,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(snapshot.data[2].username),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  color: snapshot.data[3].color,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(snapshot.data[3].username),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                          ],
                         ),
-                        Text("Pink"),
                       ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 20,
-                          width: 20,
-                          color: Colors.green,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("Green"),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 20,
-                          width: 20,
-                          color: Colors.purple,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("Purple"),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 20,
-                          width: 20,
-                          color: Colors.grey,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("Grey"),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return Text("Some Error going on.");
+                  }
+                  return CircularProgressIndicator();
+                }),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
@@ -178,12 +282,12 @@ class _ChartScreenState extends State<ChartScreen> {
                                 Container(
                                   height: 20,
                                   width: 20,
-                                  color: Colors.pink,
+                                  color: snapshot.data[0].color,
                                 ),
                                 SizedBox(
                                   width: 10,
                                 ),
-                                Text("Pink"),
+                                Text(snapshot.data[0].username),
                                 // Text(snapshot.data["userName"]),
                               ],
                             ),
@@ -195,12 +299,12 @@ class _ChartScreenState extends State<ChartScreen> {
                                 Container(
                                   height: 20,
                                   width: 20,
-                                  color: Colors.green,
+                                  color: snapshot.data[1].color,
                                 ),
                                 SizedBox(
                                   width: 10,
                                 ),
-                                Text("Green"),
+                                Text(snapshot.data[1].username),
                               ],
                             ),
                             SizedBox(
@@ -211,12 +315,12 @@ class _ChartScreenState extends State<ChartScreen> {
                                 Container(
                                   height: 20,
                                   width: 20,
-                                  color: Colors.purple,
+                                  color: snapshot.data[2].color,
                                 ),
                                 SizedBox(
                                   width: 10,
                                 ),
-                                Text("Purple"),
+                                Text(snapshot.data[2].username),
                               ],
                             ),
                             SizedBox(
@@ -227,12 +331,12 @@ class _ChartScreenState extends State<ChartScreen> {
                                 Container(
                                   height: 20,
                                   width: 20,
-                                  color: Colors.grey,
+                                  color: snapshot.data[3].color,
                                 ),
                                 SizedBox(
                                   width: 10,
                                 ),
-                                Text("Grey"),
+                                Text(snapshot.data[3].username),
                               ],
                             ),
                             SizedBox(
@@ -325,22 +429,11 @@ class _ChartScreenState extends State<ChartScreen> {
                 Expense(
                   change.doc["userName"],
                   double.parse(change.doc["amount"]),
-                  Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-                      .withOpacity(1.0),
+                  Colors.pink,
                 ),
               );
               users.add(change.doc["userName"]);
             }
-            // (int.parse(change.doc['amount']) > 0)
-            //     ? null
-            //     : _expense.add(
-            //   Expense(
-            //     change.doc["userName"],
-            //     double.parse(change.doc["amount"]),
-            //     Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-            //         .withOpacity(1.0),
-            //   ),
-            // );
           },
         );
       },
@@ -351,27 +444,84 @@ class _ChartScreenState extends State<ChartScreen> {
       _expenseChrt.add(Expense(
         element,
         0,
-        Color(
-          (math.Random().nextDouble() * 0xFFFFFF).toInt(),
-        ).withOpacity(1.0),
+        colorList.elementAt(
+          (math.Random().nextInt(colorList.length)),
+        ),
       ));
     });
 
-    for(var exp in _expense){
-      for(var expCh in _expenseChrt){
-        if(exp.username == expCh.username){
+    for (var exp in _expense) {
+      for (var expCh in _expenseChrt) {
+        if (exp.username == expCh.username) {
           expCh.amount += exp.amount;
         }
       }
     }
 
-    // Income de rh h
-    // _expenseChrt.sort((a, b) => b.amount.compareTo(a.amount));
-
-
     _expenseChrt.sortReversed((a, b) => b.amount.compareTo(a.amount));
 
+    _expense = [];
+    if (_expenseChrt.length > 3) {
+      for (var i = 0; i < _expenseChrt.length; i++) {
+        if (i > 3) {
+          _expenseChrt.elementAt(3).amount += _expenseChrt.elementAt(i).amount;
+          _expenseChrt.elementAt(3).username = 'Others';
+          if(i == (_expenseChrt.length-1))
+            _expense.insert(3, _expenseChrt.elementAt(3));
+        } else {
+          _expense.add(_expenseChrt.elementAt(i));
+        }
+      }
+    }
+    _expenseChrt = [];
+    return _expense;
+  }
 
+  Future getIncomeList() async {
+    late List<Income> _income = [];
+    late List<Income> _incomeChrt = [];
+    Set<String> users = Set();
+    CollectionReference fireStore =
+        FirebaseFirestore.instance.collection("budgetTree");
+    await fireStore.get().then(
+      (res) {
+        res.docChanges.forEach(
+          (change) {
+            if (int.parse(change.doc['amount']) > 0) {
+              _income.add(
+                Income(
+                  change.doc["userName"],
+                  double.parse(change.doc["amount"]),
+                  Colors.pink,
+                ),
+              );
+              users.add(change.doc["userName"]);
+            }
+          },
+        );
+      },
+      onError: (e) => print("Error completing: $e"),
+    );
+
+    users.forEach((element) {
+      _incomeChrt.add(Income(
+        element,
+        0,
+        colorList.elementAt(
+          (math.Random().nextInt(colorList.length)),
+        ),
+      ));
+    });
+
+    for (var inc in _income) {
+      for (var incCh in _incomeChrt) {
+        if (inc.username == incCh.username) {
+          incCh.amount += inc.amount;
+        }
+      }
+    }
+
+    _incomeChrt.sort((a, b) => b.amount.compareTo(a.amount));
 
 // FOr Income
     // if(_expenseChrt.length > 3){
@@ -383,20 +533,21 @@ class _ChartScreenState extends State<ChartScreen> {
     //     }
     //   }
     // }
-    _expense = [];
-    if(_expenseChrt.length > 3){
-      for(var i = 0; i < _expenseChrt.length; i++){
-        if (i > 3){
-          _expenseChrt.elementAt(3).amount += _expenseChrt.elementAt(i).amount;
-          _expenseChrt.elementAt(3).username = 'Others';
-          _expense.insert(3, _expenseChrt.elementAt(3));
-        }
-        else{
-          _expense.add(_expenseChrt.elementAt(i));
+
+    _income = [];
+    if (_incomeChrt.length > 3) {
+      for (var i = 0; i < _incomeChrt.length; i++) {
+        if (i > 3) {
+          _incomeChrt.elementAt(3).amount += _incomeChrt.elementAt(i).amount;
+          _incomeChrt.elementAt(3).username = 'Others';
+          if(i == (_incomeChrt.length-1))
+            _income.insert(3, _incomeChrt.elementAt(3));
+        } else {
+          _income.add(_incomeChrt.elementAt(i));
         }
       }
     }
-    _expenseChrt = [];
-    return _expense;
+    _incomeChrt = [];
+    return _income;
   }
 }
